2221f5efbc4fe5c73a9123e1aeee12d728a5169f
VolhaShyrko <olgashyrko@gmail.com>
2016-08-23 10:01:12 +0300

    first commit

---
+++ ConsoleApplication4/InputHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication4
+{
+    public class InputHandler
+    {
+        public static List<string> GetList()
+        {
+            List<string> userList = new List<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input != "#")
+                {
+                    userList.Add(input);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return userList;
+        }
+
+        public static void DisplayListFirstLetters(List<string> list)
+        {
+            Console.WriteLine();
+
+            foreach (var item in list)
+            {
+                try
+                {
+                    Console.WriteLine(item.Substring(0, 1));
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    // DoNothing
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some text");
+            Console.WriteLine("After input finish enter '#'");
+
+            var userList = InputHandler.GetList();
+
+            InputHandler.DisplayListFirstLetters(userList);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
